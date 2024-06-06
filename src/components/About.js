fe5adfb2f604eefb7c64123de26638abee0a09a6
Ch3mson <bensonyan778@hotmail.com>
2024-06-05 22:22:40 -0400

    updatedAbout

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ export function About() {
             </div>
 
             <div class="container mx-auto w-3/5 p-4">
-                <div className="grid items-center grid-cols-2 gap-4 relative overflow-hidden py-24 lg:py-32 lg:grid-cols-2 grid-cols-1">
+                <div className="grid items-center gap-4 relative overflow-hidden py-24 lg:py-32 lg:grid-cols-2 grid-cols-1">
                     <div className="text-center">THIS IS ITEM 1</div>
                     <div className="text-center">THIS IS ITEM 2</div>
                 </div>
