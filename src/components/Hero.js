4de373659cf49e63f9c72e2a39cbe5449fe510ae
Ch3mson <bensonyan778@hotmail.com>
2024-06-10 16:37:46 -0400

    added hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,40 +1,16 @@
 "use client";
-import React from "react";
-import { SparklesCore } from "./ui/sparkles";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
-const headingClass = "text-7xl font-extrabold inline-block";
+
+const words = `Hello there! Welcome to my personal portfolio.`;
 
 export function Hero() {
+  
   return (
-    <div className="relative w-full flex items-center justify-center overflow-hidden rounded-md mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12">
-      <div className="w-full absolute inset-0 h-screen">
-        <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={100}
-          className="w-full h-full"
-          particleColor="#FFFFFF"
-        />
-      </div>
-      <div className="relative z-20 w-full flex items-center justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 w-full h-full">
-          <div className="flex items-center justify-center order-1 md:order-1">
-            <img
-              src="/profilepic.jpg"
-              alt="Yan Benson"
-              className="w-full h-auto rounded-md"
-            />
-          </div>
-          <div className="flex flex-col items-center md:items-start justify-center space-y-4 order-2 md:order-2">
-            <h1 className={headingClass}>
-              Yan
-            </h1>
-            <h1 className={headingClass}>
-              Benson
-            </h1>
-          </div>
+    <div className="flex flex-col items-center justify-center h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="py-24 max-w-7xl lg:py-32">
+        <TextGenerateEffect words={words} />
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
         </div>
       </div>
     </div>
