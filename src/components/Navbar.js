fe97a4c09da9f861669babcebf6a2cce41592c85
Benson Yan <bensonyan778@hotmail.com>
2024-07-03 21:00:08 +0000

    major changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import Image from 'next/image';
+import head from '../../public/head.png';
+
+
 
 export function Navbar() {
-    return (
-        <nav className="fixed top-0 left-0 h-full flex flex-col items-start p-4">
-        <a href="#home" className="mb-4 text-xl font-medium text-gray-800 hover:text-gray-600">Home</a>
-        <a href="#about" className="mb-4 text-xl font-medium text-gray-800 hover:text-gray-600">About</a>
-        <a href="#projects" className="mb-4 text-xl font-medium text-gray-800 hover:text-gray-600">Projects</a>
-        <a href="#contact" className="text-xl font-medium text-gray-800 hover:text-gray-600">Contact</a>
-      </nav>
-    );
+  return (
+    <nav className="fixed top-0 left-0 h-full flex flex-col items-center gap-y-10 p-4">
+      <div className="sticky flex flex-col items-center space-y-8 mt-8 gap-10">
+      <a href="/">
+        <Image href="/" src={head} alt="Logo" width={50} height={50} />
+      </a>
+        <a href="#home" className={`text-lg font-medium text-gray-800 hover:text-[#B0D6D3]`} style={{ transform: 'rotate(-90deg)' }}>Home</a>
+        <a href="#about" className={`text-lg font-medium text-gray-800 hover:text-[#B0D6D3]`} style={{ transform: 'rotate(-90deg)' }}>About</a>
+        <a href="#projects" className={`text-lg font-medium text-gray-800 hover:text-[#B0D6D3]`} style={{ transform: 'rotate(-90deg)' }}>Projects</a>
+        <a href="#contact" className={`text-lg font-medium text-gray-800 hover:text-[#B0D6D3]`} style={{ transform: 'rotate(-90deg)' }}>Contact</a>
+      </div>
+    </nav>
+  );
 }
+
